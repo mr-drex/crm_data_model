f77d04b7e912865f444a5de761bef90916bb4fab
Aditya <21051112@kiit.ac.in>
2024-04-29 14:07:15 +0530

    cust info table is created

---
+++ DB_Scripts/DDL/001.create-Cust_info.sql
@@ -0,0 +1,4 @@
+CREATE TABLE Cust_info (ID INT PRIMARY KEY,
+                                       F_name VARCHAR(50),
+                                              L_name VARCHAR(50),
+                                                     Age INT , Gender VARCHAR(10));
